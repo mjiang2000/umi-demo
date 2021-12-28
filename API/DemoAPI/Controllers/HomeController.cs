dda6bdff11ebcfcedd925910b28f1303c13340e5
Jack <jack.jiang@beeshop.chat>
2021-12-28 11:04:22 -0500

    clean up

--- API/DemoAPI/Controllers/HomeController.cs
+++ API/DemoAPI/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web.Resource;
 using Microsoft.Identity.Web;
 using Newtonsoft.Json;
--- API/DemoAPI/Startup.cs
+++ API/DemoAPI/Startup.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
 using System.Diagnostics;
 using System.Collections.Generic;
